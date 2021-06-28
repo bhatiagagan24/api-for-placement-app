361d3eb4736c44926740c2efcadd154471efbd10
bhatiagagan24 <47947926+bhatiagagan24@users.noreply.github.com>
2021-06-28 21:01:05 +0530

    commenting out all the main functions and code

--- app.py
+++ app.py
@@ -19,33 +19,35 @@ app = Flask(__name__)
 url = "https://amity.edu/placement/upcoming-recruitment.asp"
 
 
-def updateLists():
-    returnJson = []
-    cursor = mycol.find({})
-    for links in cursor:
-        href = links['urLinkId']
-        name = links['name']
-        name = name.replace("\\u002e", ".")
-        href = href.replace("\\u002e", ".")
-        href = "https://amity.edu/placement/" + href
-        returnJson.append({"title": name, "href": href})
-    return returnJson
+# def updateLists():
+#     returnJson = []
+#     cursor = mycol.find({})
+#     for links in cursor:
+#         href = links['urLinkId']
+#         name = links['name']
+#         name = name.replace("\\u002e", ".")
+#         href = href.replace("\\u002e", ".")
+#         href = "https://amity.edu/placement/" + href
+#         returnJson.append({"title": name, "href": href})
+#     return returnJson
 
-
-
-@app.route('/')
+@app.route('/)
 def home():
-    timeCheck = unixTimeStamp.checkAndUpdateTime() 
-    if timeCheck == 0:
-        returnJson = updateLists()
-        json_object = json.dumps(returnJson, indent=4)
-        return json_object
-    else:
-        newScraped = scraper.scraper(url)
-        returnJson = updateLists()
-        json_object = json.dumps(returnJson, indent=4)
-        return json_object
-    return [{"title": "Programming error"}]
+    return "App switched off"
+
+# @app.route('/')
+# def home():
+#     timeCheck = unixTimeStamp.checkAndUpdateTime() 
+#     if timeCheck == 0:
+#         returnJson = updateLists()
+#         json_object = json.dumps(returnJson, indent=4)
+#         return json_object
+#     else:
+#         newScraped = scraper.scraper(url)
+#         returnJson = updateLists()
+#         json_object = json.dumps(returnJson, indent=4)
+#         return json_object
+#     return [{"title": "Programming error"}]
 
     
 
